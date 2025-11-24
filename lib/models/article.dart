b3a99ec2796d7a7cb3556a285e841a213366c8fe
BatteryScentedCandle <aldeasandwich@gmail.com>
2025-11-24 11:51:49 +0800

    added basic model class for articles and empty files

---
+++ lib/models/article.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class Article {
+  String articleName;
+  Image articleImage;
+  DateTime articleDate;
+
+  Article({
+    required this.articleName,
+    required this.articleImage,
+    required this.articleDate
+});
+}
