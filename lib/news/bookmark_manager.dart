a40bbdd3738102f04a3eec8c95014e63b31c6255
RedFlameKen <nettolacaba@gmail.com>
2025-12-01 15:06:46 +0800

    feat: add bookmark manager

---
+++ lib/news/bookmark_manager.dart
@@ -0,0 +1,184 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:watchmans_gazette/news/news_api_requester.dart';
+
+class BookmarkManager {
+  static final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  static const COLLECTION_NAME = "bookmarks";
+
+  static Future<void> addBookmark({
+    required NewsItem newsItem,
+    required Function(BookmarkItem) onSuccess,
+    Function(String)? onFail,
+  }) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      if (onFail != null) {
+        onFail("user is not logged in yet");
+      }
+      return;
+    }
+
+    int curPos = 0;
+    final query = await db
+        .collection(COLLECTION_NAME)
+        .doc(user.uid)
+        .collection(COLLECTION_NAME)
+        .get();
+
+    curPos = query.size;
+
+    db
+        .collection(COLLECTION_NAME)
+        .doc(user.uid)
+        .collection(COLLECTION_NAME)
+        .add({
+          "dateAdded": DateTime.now(),
+          "pos": curPos,
+          "news": {
+            "id": newsItem.id,
+            "title": newsItem.title,
+            "description": newsItem.description,
+            "language": newsItem.language,
+            "publishedAt": newsItem.publishedAt,
+            "source": newsItem.source,
+            "site": newsItem.site,
+            "category": newsItem.category,
+            "country": newsItem.country,
+            "author": newsItem.author,
+            "keywords": newsItem.keywords,
+            "newsImage": {
+              "img": newsItem.newsImage.img,
+              "video": newsItem.newsImage.video,
+            },
+            "contentApi": newsItem.contentApi,
+            "sdgNumber": newsItem.sdgNumber,
+          },
+        })
+        .then((value) {
+          final bookmark = BookmarkItem(
+            uuid: value.id,
+            dateAdded: DateTime.now(),
+            pos: curPos,
+            newsItem: newsItem,
+          );
+
+          onSuccess(bookmark);
+        })
+        .catchError((error) {
+          if (onFail != null) {
+            onFail("$error");
+          }
+        });
+  }
+
+  static Future<void> deleteBookmark({
+    required BookmarkItem bookmark,
+    required Function(String) onSuccess,
+    Function(String)? onFail,
+  }) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      if (onFail != null) {
+        onFail("user is not logged in yet");
+      }
+      return;
+    }
+
+    db
+        .collection(COLLECTION_NAME)
+        .doc(user.uid)
+        .collection(COLLECTION_NAME)
+        .doc(bookmark.uuid)
+        .delete()
+        .then(
+          (value) {
+            onSuccess("bookmark deleted");
+          },
+          onError: (error) {
+            if (onFail != null) {
+              onFail("$error");
+            }
+          },
+        );
+  }
+
+  static Future<void> getBookmarks({
+    required BookmarkItem bookmark,
+    required Function(List<BookmarkItem>) onSuccess,
+    Function(String)? onFail,
+  }) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      if (onFail != null) {
+        onFail("user is not logged in yet");
+      }
+      return;
+    }
+
+    final QuerySnapshot<Map<String, dynamic>> query;
+    try {
+      query = await db
+          .collection(COLLECTION_NAME)
+          .doc(user.uid)
+          .collection(COLLECTION_NAME)
+          .get();
+    } catch (e) {
+      if (onFail != null) {
+        onFail("$e");
+      }
+      return;
+    }
+
+    final docs = query.docs;
+    List<BookmarkItem> bookmarks = List.generate(docs.length, (index) {
+      final doc = docs.elementAt(index);
+      final newsImage = NewsImage(
+        img: doc.get(FieldPath(["newsImage", "img"])),
+        video: doc.get(FieldPath(["newsImage", "video"])),
+      );
+      final newsItem = NewsItem(
+        id: doc.get("id"),
+        title: doc.get("title"),
+        description: doc.get("description"),
+        language: doc.get("language"),
+        publishedAt: doc.get("publishedAt"),
+        source: doc.get("source"),
+        site: doc.get("site"),
+        category: doc.get("category"),
+        country: doc.get("country"),
+        author: doc.get("author"),
+        keywords: doc.get("keywords"),
+        contentApi: doc.get("contentApi"),
+        newsImage: newsImage,
+        sdgNumber: doc.get("sdgNumber"),
+      );
+      return BookmarkItem(
+        uuid: doc.id,
+        dateAdded: doc.get("dateAdded"),
+        pos: doc.get("pos"),
+        newsItem: newsItem,
+      );
+    });
+    bookmarks.sort((a, b) => a.pos - b.pos);
+
+    onSuccess(bookmarks);
+  }
+}
+
+class BookmarkItem {
+  String uuid;
+  DateTime dateAdded;
+  int pos;
+  NewsItem newsItem;
+
+  BookmarkItem({
+    required this.uuid,
+    required this.dateAdded,
+    required this.pos,
+    required this.newsItem,
+  });
+}
