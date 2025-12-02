5d8b6989151e496ba0c4217dd4cd80810e39079e
RedFlameKen <nettolacaba@gmail.com>
2025-12-02 09:59:45 +0800

    fix: make content selectable

--- lib/screens/content_view_page.dart
+++ lib/screens/content_view_page.dart
@@ -36,59 +36,61 @@ class ContentViewPage extends StatelessWidget {
           ),
           Padding(
             padding: .only(left: 12, right: 12, bottom: 12),
-            child: Column(
-              crossAxisAlignment: .start,
-              children: [
-                Text(
-                  newsContent.title,
-                  style: TextStyle(fontSize: 24, fontWeight: .bold),
-                ),
-                Row(
-                  crossAxisAlignment: .center,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.account_circle, size: 16),
-                        SizedBox(width: 4),
-                        Text(_getAuthor()),
-                      ],
-                    ),
-                    SizedBox(width: 12),
-                    Row(
-                      children: [
-                        Icon(Icons.calendar_month, size: 16),
-                        SizedBox(width: 4),
-                        Text(newsContent.publishedAt),
-                      ],
-                    ),
-                  ],
-                ),
-                InkWell(
-                  child: Text(
-                    "View source...",
-                    textAlign: .start,
-                    style: TextStyle(color: Colors.pinkAccent),
+            child: SelectionArea(
+              child: Column(
+                crossAxisAlignment: .start,
+                children: [
+                  Text(
+                    newsContent.title,
+                    style: TextStyle(fontSize: 24, fontWeight: .bold),
                   ),
-                  onTapUp: (details) async {
-                    final uri = Uri.parse(newsContent.source);
-                    if (!await launchUrl(uri)) {
-                      throw Exception("Could not open $uri");
-                    }
-                  },
-                ),
-                const Padding(padding: .all(5)),
-                Text(
-                  newsContent.description,
-                  textAlign: .justify,
-                  style: TextStyle(fontSize: 16),
-                ),
-                const Padding(padding: .all(5)),
-                Text(
-                  newsContent.content,
-                  textAlign: .justify,
-                  style: TextStyle(fontSize: 16),
-                ),
-              ],
+                  Row(
+                    crossAxisAlignment: .center,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.account_circle, size: 16),
+                          SizedBox(width: 4),
+                          Text(_getAuthor()),
+                        ],
+                      ),
+                      SizedBox(width: 12),
+                      Row(
+                        children: [
+                          Icon(Icons.calendar_month, size: 16),
+                          SizedBox(width: 4),
+                          Text(newsContent.publishedAt),
+                        ],
+                      ),
+                    ],
+                  ),
+                  InkWell(
+                    child: Text(
+                      "View source...",
+                      textAlign: .start,
+                      style: TextStyle(color: Colors.pinkAccent),
+                    ),
+                    onTapUp: (details) async {
+                      final uri = Uri.parse(newsContent.source);
+                      if (!await launchUrl(uri)) {
+                        throw Exception("Could not open $uri");
+                      }
+                    },
+                  ),
+                  const Padding(padding: .all(5)),
+                  Text(
+                    newsContent.description,
+                    textAlign: .justify,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const Padding(padding: .all(5)),
+                  Text(
+                    newsContent.content,
+                    textAlign: .justify,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
